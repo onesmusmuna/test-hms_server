5b58991ddd545e4f9d5936bc68b789145d33e85b
onesmusmuna <onesmusmuna@gmail.com>
2024-02-20 15:12:24 +0300

    custom return

---
+++ src/util/cr.util.ts
@@ -0,0 +1,22 @@
+class cr {
+  static str(status: TStatus, msg: string): IReturn {
+    return { status, msg };
+  }
+
+  static load(status: TStatus, load: TLoad): IReturn {
+    return { status, load };
+  }
+
+  static strLoad(status: TStatus, msg: string, load: TLoad): IReturn {
+    return { status, msg, load };
+  }
+}
+
+type TStatus = "ok" | "bad" | "fail";
+type TLoad = { [index: string]: unknown };
+
+interface IReturn {
+  status: TStatus;
+  msg?: string;
+  load?: TLoad;
+}
