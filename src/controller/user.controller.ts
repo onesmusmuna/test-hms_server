f20fe1d891a018bf81292c6f18c90472068e48b4
onesmusmuna <onesmusmuna@gmail.com>
2024-02-20 15:20:42 +0300

    router structure

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+
+import db from "../util/db.util";
+
+export function register(req: Request, res: Response) {}
+
+export function login(req: Request, res: Response) {}
+
+export function logout(req: Request, res: Response) {}
---
+++ src/util/router/user.router.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const routerUser = Router();
+
+export default routerUser;
